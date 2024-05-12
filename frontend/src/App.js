7dfacfd6ca15d5826e1792c7f357dd78027dcde9
IT21330896 <IT21330896@my.sliit.lk>
2024-05-12 16:34:36 +0530

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WorkoutPlansPage from './Pages/WorkoutPlansPage';
 import WorkoutPlanList from './Pages/WorkoutPlanList';
+import WorkoutPlansPage from './Pages/WorkoutPlansPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/api/workout-plans" element={<WorkoutPlansPage />} />
+        <Route path="/api/workout-plan" element={<WorkoutPlansPage />} />
         <Route path="/workout-plan-list" element={<WorkoutPlanList />} />
       </Routes>
     </Router>
